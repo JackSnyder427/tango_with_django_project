7fdba2f75a8aff210819b4b027bf148c6c5259ee
JackSnyder427 <3147104s@student.gla.ac.uk>
2026-01-22 13:44:17 +0000

    Created 'rango' subdirectory and added new view

--- tango_with_django/urls.py
+++ tango_with_django/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from rango import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('rango/', include('rango.urls')), # Sends urls beginning with rango/ to urls.py in rango subdirectory
     path('admin/', admin.site.urls),
 ]
